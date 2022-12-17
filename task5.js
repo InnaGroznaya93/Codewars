76e814a82b99924c32b84cef51a86bc07b42d41c
Inna Groznaya <inna2011@tut.by>
2022-12-17 23:56:07 +0300

    resolved kata about 'hand-made' filter method

---
+++ task5.js
@@ -0,0 +1,12 @@
+Array.prototype.filter = function (callback, context) {
+    let result = []
+    let l = this.length
+    for(let i = 0; i < l; i++) {
+        if(i in this) {
+            if(callback.call(context, this[i], i, this)) {
+                result.push(this[i])
+            }
+        }
+    }
+    return result
+}
