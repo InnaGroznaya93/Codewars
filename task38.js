7142114e6795e351ce42f3399cabb32a19eea56a
Inna Groznaya <inna2011@tut.by>
2023-09-12 00:52:51 +0300

    task39 hof

--- task38.js
+++ task38.js
@@ -1,5 +1,4 @@
 function getPlanetName(id){
-    debugger
     var name;
     switch(id){
       case 1:
@@ -32,3 +31,40 @@ function getPlanetName(id){
   }
 
   console.log(getPlanetName(2))
+
+
+  function greet(language) {
+  
+    //IP_ADDRESS_INVALID - not a valid ipv4 or ipv6 ip address
+   //IP_ADDRESS_NOT_FOUND - ip address not in the database
+   //IP_ADDRESS_REQUIRED - no ip address was supplied
+
+   debugger
+     
+       const greetings = { 
+    "english": "Welcome",
+    "czech": "Vitejte",
+    "danish": "Velkomst",
+    "dutch": "Welkom",
+    "estonian": "Tere tulemast",
+    "finnish": "Tervetuloa",
+    "flemish": "Welgekomen",
+    "french": "Bienvenue",
+    "german": "Willkommen",
+    "irish": "Failte",
+    "italian": "Benvenuto",
+    "latvian": "Gaidits",
+    "lithuanian": "Laukiamas",
+    "polish": "Witamy",
+    "spanish": "Bienvenido",
+    "swedish": "Valkommen",
+    "welsh": "Croeso",
+   }
+     
+
+     return greetings[language]
+   }
+
+   console.log(greet("dutch"))
+
+   
